b5dab1a934af52666e9b3a9a4c440bcf575da176
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-05-23 17:39:20 +0300

    Добавил проект

---
+++ PrimeStore/PrimeStore/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+
+var app = builder.Build();
+
+app.UseDeveloperExceptionPage();
+app.UseStatusCodePages();
+
+app.UseStaticFiles();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
