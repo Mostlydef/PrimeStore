d2cc6ad0adb4f0c603d5eecf50aaea968fa8a0b5
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-06-06 21:55:50 +0300

    Добавил интерфейс

---
+++ PrimeStore/PrimeStore/Data/Interfaces/IAllFile.cs
@@ -0,0 +1,10 @@
+﻿using PrimeStore.Data.Models;
+
+namespace PrimeStore.Data.Interfaces
+{
+    public interface IAllFile
+    {
+        IEnumerable<Models.File> Files { get; } // добавить set
+
+    }
+}
