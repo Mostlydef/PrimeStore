85aa51b43de7734107dc06df47b16e6ec630baf5
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-06-06 22:16:45 +0300

    Добавил сервис

--- PrimeStore/PrimeStore/Program.cs
+++ PrimeStore/PrimeStore/Program.cs
@@ -1,17 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PrimeStore.Data;
+using PrimeStore.Data.Interfaces;
+using PrimeStore.Data.Repositiory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-
-
+builder.Services.AddDbContext<PrimeStoreContext>(options => options.UseNpgsql());
+builder.Services.AddTransient<IAllFile, HomeRepository>();
 var app = builder.Build();
 
 app.UseDeveloperExceptionPage();
 app.UseStatusCodePages();
 
 app.UseStaticFiles();
-
-app.MapGet("/", () => "Hello World!");
 
 app.MapControllerRoute(
     name: "default",
