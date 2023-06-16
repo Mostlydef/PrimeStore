5c6492ea8f7c64aaf2e8cdfee10d806a22f2e5ed
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-06-16 19:12:53 +0300

    Добавление реализации интерфейсов

--- PrimeStore/PrimeStore/Data/Repositiory/HomeRepository.cs
+++ PrimeStore/PrimeStore/Data/Repositiory/HomeRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PrimeStore.Data.Interfaces;
+using PrimeStore.Data.Models;
 
 namespace PrimeStore.Data.Repositiory
 {
-    public class HomeRepository : IAllFile
+    public class HomeRepository : IAllFile, IAllFolder
     {
         private readonly PrimeStoreContext _context;
 
@@ -13,5 +14,44 @@ namespace PrimeStore.Data.Repositiory
         }
 
         public IEnumerable<Models.File> Files => _context.Files.Include(c => c.Folder);
+
+        public IEnumerable<Folder> Folders
+        {
+            get
+            {
+                return _context.Folders.Include(c => c.User);
+            }
+        }
+
+        public Folder Folder
+        {
+            set
+            {
+                _context.Folders.Add(value);
+                _context.SaveChanges();
+            }
+        }
+
+        public Models.File File
+        {
+            set
+            {
+                _context.Files.Add(value);
+                _context.SaveChanges();
+            }
+        }
+
+        public bool SetFileInBasket(int id)
+        {
+            Models.File file = _context.Files.FirstOrDefault(p => p.Id == id);
+            if(file != null)
+            {
+                file.InBasket = true;
+                _context.Files.Update(file);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
     }
 }
