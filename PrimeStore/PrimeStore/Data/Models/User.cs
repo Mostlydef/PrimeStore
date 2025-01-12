97a7c22dfc145391c99a46eb7cd8f55da511b562
Mostlydef <v.danilevitch2013@yandex.ru>
2025-01-12 23:36:18 +0300

    изменил модель user, убрал лишние стобцы

---
+++ PrimeStore/PrimeStore/Data/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrimeStore.Data.Models;
+
+public partial class User : IdentityUser
+{
+    public virtual ICollection<Folder> Folders { get; set; } = new List<Folder>();
+}
