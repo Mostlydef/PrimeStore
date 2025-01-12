af8e582808e857f72f10eeedcce355076e295d7d
Mostlydef <v.danilevitch2013@yandex.ru>
2025-01-12 23:41:50 +0300

    поменял id на guid

---
+++ PrimeStore/PrimeStore/Data/Models/SharedFolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeStore.Data.Models;
+
+public partial class SharedFolder
+{
+    public string Guid { get; set; }
+
+    public int FolderId { get; set; }
+
+    public virtual Folder Folder { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
